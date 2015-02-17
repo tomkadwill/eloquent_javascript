9a958f023231a8f4403375d23cbe00df94768500
Tom Kadwill <tomkadwill@notonthehighstreet.com>
2015-02-17 15:09:44 +0000

    Refactored chap 5, ex2

--- chap5.js
+++ chap5.js
@@ -11,10 +11,28 @@ console.log(reduce(arrays, function(a, b) {
 }, []));
 
 //ex2 - Mother-child age difference
-// TODO: redo in a more elegant way
+
+function map(array, transform, nameObj) {
+  return transform(averageAges(array, nameObj));
+}
+
 function average(array) {
   function plus(a, b) { return a + b; }
   return array.reduce(plus) / array.length;
+}
+
+function averageAges(array, nameObj) {
+  for(name in nameObj) {
+    var person = nameObj[name];
+    var personBorn = person.born;
+    var mother = nameObj[person.mother];
+    if(mother != null) {
+      var motherBorn = mother.born;
+      var difference = personBorn - motherBorn;
+      array.push(difference);
+    }
+  }
+  return array;
 }
 
 var byName = {};
@@ -22,14 +40,6 @@ ancestry.forEach(function(person) {
   byName[person.name] = person;
 });
 
-arrayOfAverages = []
-for(name in byName) {
-  person = byName[name]
-  personBorn = person.born
-  mother = byName[person.mother];
-  if(mother != null) {
-  	motherBorn = mother.born
-    var difference = personBorn - motherBorn;
-    arrayOfAverages.push(difference);
-  }
-}
+console.log(map([], average, byName));
+
+//ex3 - Historical life expectancy
